319db9ec384bf2d4cb84d4045656018de06ffac3
Muhammad Sulayman Haydar <mohamad.sleimanhaidar@gmail.com>
2024-03-20 15:49:37 +0200

    build button component set

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <p className="bg-red-500 px-5 font-bold">he,lo</p>
+      <Button variant="default" size="lg">
+        fewfewfwefwe
+      </Button>
     </div>
   );
 }
